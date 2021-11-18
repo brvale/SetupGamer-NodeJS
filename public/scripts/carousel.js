6f8f37db785f5c07d2ab360ea5ef909b61a9228b
brvale <brunorichard2531@gmail.com>
2021-11-17 22:56:54 -0300

    Criando prevButton para o carrossel

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,21 +1,32 @@
 //carrosel
 const slides = document.querySelectorAll('[data-js="carousel__item"]')
 const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+const prevButton = document.querySelector('[data-js="carousel__button--prev"]')
 
 let currentSlideIndex = 0
 
-nextButton.addEventListener('click', () => {
-    if(currentSlideIndex === slides.length - 1) {
-        currentSlideIndex = 0
-    } else{
-        currentSlideIndex++
-    }
-    
+const manipulateSlidesClasses = correctSlideIndex => {
     slides.forEach(slide => {
         slide.classList.remove('carousel__item--visible')
     })
 
-    slides[currentSlideIndex].classList.add('carousel__item--visible')
+    slides[correctSlideIndex].classList.add('carousel__item--visible')
+}
+
+nextButton.addEventListener('click', () => {
+    const correctSlideIndex = currentSlideIndex === slides.length - 1
+        ? currentSlideIndex = 0
+        : ++currentSlideIndex
+    
+    manipulateSlidesClasses(correctSlideIndex)
+})
+
+prevButton.addEventListener('click', () => {
+    const correctSlideIndex = currentSlideIndex === 0
+        ? currentSlideIndex = slides.length - 1
+        : --currentSlideIndex
+    
+    manipulateSlidesClasses(correctSlideIndex)
 })
 
 
