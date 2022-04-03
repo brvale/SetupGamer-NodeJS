8e85c897ea03339b09dd801206fb51c2e3c4b3bd
GabrielMiranda21 <gabrielmirand204@gmail.com>
2022-04-03 20:42:56 -0300

    Correção de código

--- middleware/campoValidacao.js
+++ middleware/campoValidacao.js
@@ -2,29 +2,31 @@ const { check } = require("express-validator");
 
 const validarCampos = [
     check(nome)
-        .notEmpty().withMessage("Deve preencher o nome").bail()
+        .notEmpty().withMessage("Deve preencher o campo nome").bail()
         .isString().withMessage("Apenas caracteres letras"),
     check(sobrenome)
-        .notEmpty().withMessage("Deve preencher o sobrenome").bail()
+        .notEmpty().withMessage("Deve preencher o campo sobrenome").bail()
         .isString().withMessage("Apenas caracteres letras"),
     check(email)
-        .notEmpty().withMessage("Deve preencher o email").bail()
+        .notEmpty().withMessage("Deve preencher o campo email").bail()
         .isEmail().withMessage("Deve preencher com um email válido"),
     check(dataDeNascimento)
-        .notEmpty().withMessage("Deve preencher a data de nascimento").bail()
+        .notEmpty().withMessage("Deve preencher o campo data de nascimento").bail()
         .isNumeric().withMessage("Deve preencher com números"),
     check(genero)
-        .notEmpty().withMessage("Deve preencher o genero").bail()
+        .notEmpty().withMessage("Deve preencher o campo genero").bail()
         .isString().withMessage("Deve preencher com caracteres"),
     check(cpf)
-        .notEmpty().withMessage("Deve preencher o cpf").bail()
+        .notEmpty().withMessage("Deve preencher o campo cpf").bail()
         .isNumeric().withMessage("Deve preencher com números"),
     check(telefone)
-        .notEmpty().withMessage("Deve preencher o telefone").bail()
-        .isNumeric().withMessage("Deve preencher com números"),
+        .notEmpty().withMessage("Deve preencher o campo telefone").bail()
+        .isNumeric().withMessage("Deve preencher com números"), 
     check(senha)
-        .notEmpty().withMessage("Deve preencher o telefone").bail()
+        .notEmpty().withMessage("Deve preencher o campo senha").bail()
         .isString().withMessage("Deve preencher com letras e números")
+        .isLength({ min: 10, max: 20 }).withMessage("Esse campo deve possuir no minimo 10 caracteres")
 ]
 
 module.exports = validarCampos;
+
