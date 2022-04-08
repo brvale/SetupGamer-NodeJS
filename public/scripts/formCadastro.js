0bb4ed96ac54c560da484601f1fa832a202f047a
brvale <brunorichard2531@gmail.com>
2022-04-07 22:18:25 -0300

    Finalizando cadastro front

--- public/scripts/formCadastro.js
+++ public/scripts/formCadastro.js
@@ -1,26 +1,24 @@
-const btnPF = document.querySelector('[data-js="btnPF"]').focus();
-const btnPJ = document.querySelector('[data-js="btnPJ"]');
-const formPF = document.querySelector('[data-js="form-content-PF"]');
-const formPJ = document.querySelector('[data-js="form-content-PJ"]');
+const btnPF = document.getElementById('btnPF');
+const btnPJ = document.getElementById('btnPJ');
+const formPF = document.getElementById('form-content--PF');
+const formPJ = document.getElementById('form-content--PJ');
 
 btnPF.addEventListener('click', () => {
-    console.log('ativo')
-    /* if(formPJ.classList.contains('form-content--visible')) {
+    if(formPJ.classList.contains('form-content--visible')) {
         formPJ.classList.remove('form-content--visible');
         formPF.classList.add('form-content--visible');
     } else {
         formPF.classList.add('form-content--visible');
-    } */
+    }
 });
 
 btnPJ.addEventListener('click', () => {
-    console.log('desativo')
-    /* if(formPF.classList.contains('form-content--visible')) {
+    if(formPF.classList.contains('form-content--visible')) {
         formPF.classList.remove('form-content--visible');
         formPJ.classList.add('form-content--visible');
     } else {
         formPJ.classList.add('form-content--visible');
-    } */
+    }
 });
 
 
