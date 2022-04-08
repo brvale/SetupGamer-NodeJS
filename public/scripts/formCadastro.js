e213193292107f9959fbf50f88fbac2045251ea6
GabrielMiranda21 <gabrielmirand204@gmail.com>
2022-04-07 22:52:26 -0300

    adicionando foco no botões de cadastro pf e pj

--- public/scripts/formCadastro.js
+++ public/scripts/formCadastro.js
@@ -2,11 +2,13 @@ const btnPF = document.getElementById('btnPF');
 const btnPJ = document.getElementById('btnPJ');
 const formPF = document.getElementById('form-content--PF');
 const formPJ = document.getElementById('form-content--PJ');
+btnPF.focus();
 
 btnPF.addEventListener('click', () => {
     if(formPJ.classList.contains('form-content--visible')) {
         formPJ.classList.remove('form-content--visible');
         formPF.classList.add('form-content--visible');
+        btnPF.classList.add('btn-form--active');
     } else {
         formPF.classList.add('form-content--visible');
     }
@@ -16,7 +18,10 @@ btnPJ.addEventListener('click', () => {
     if(formPF.classList.contains('form-content--visible')) {
         formPF.classList.remove('form-content--visible');
         formPJ.classList.add('form-content--visible');
+        btnPJ.classList.add('btn-form--active');
     } else {
         formPJ.classList.add('form-content--visible');
+
+
     }
 });     
