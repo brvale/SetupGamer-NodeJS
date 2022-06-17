63ea8d753fd65e03a0fe6c0f1a3e46130a1fb5e8
GabrielMiranda21 <gabrielmirand204@gmail.com>
2022-06-17 17:35:31 -0300

    corrigido estrutura da controller com as rotas, criado nova view de feedback de compra finalizada

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const { EnderecoEntrega } = require('../database/models');
-const { Pedidos } = require('../database/models');
+
+const { Frete, Tipo_Pagamento, ListaProduto, Pedido } = require('../database/models');
 
 module.exports = {
     endereco: async (req, res, next) => {
@@ -54,8 +55,57 @@ const enderecoUsuario = await EnderecoEntrega.findOne({
         res.send('/checkout/endereco')
     },
 
-    /*compra: async (req, res, next) => {
+    somarTotalPedido: async function(req, res, next) {
+        const { produtos, frete } = req.session;
+    
+        const dadosFrete = await Frete.findOne({ 
+            where: { 
+                id_frete: frete
+            }
+        })
+    
+        let totalProdutos = 0;
+    
+        produtos.forEach(produto => {
+            totalProdutos += produto.valor * produto.quantidade;
+        })
+    
+        const totalGeral = dadosFrete.valor + totalProdutos;
+        req.session.totalGeral = totalGeral;
+    
+        res.render("pagamento", { title: "Forma de pagamento", totalProdutos, totalGeral, frete: dadosFrete.valor });
+    },
 
-        res.send('/checkout/frete')
-    }*/
+    pagamento: (req, res, next) => {
+        req.session.frete = req.body.transporte;
+        res.redirect('/checkout/pagamento');
+    },
+
+
+    pedido: async (req, res, next) => {
+        req.session.pagamento = req.body.radio;
+    
+            console.log(req.session)
+        const novoPedido = await Pedido.create({ 
+            id_cliente: req.session.usuario.id_cliente,
+            id_pagamento: req.session.pagamento,
+            id_endereco: req.session.endereco.id_endereco,
+            id_frete: req.session.frete,
+            valor: req.session.totalGeral
+        })
+    
+        req.session.produtos.forEach(async produto => {
+            const total = produto.valor * produto.quantidade;
+    
+            await ListaProduto.create({
+                id_pedido: novoPedido.id_pedido,
+                id_produto: produto.id_produto,
+                quantidade: produto.quantidade,
+                valor: total
+            }).catch(console.trace)
+        })
+    
+        //res.send('deu certo')
+        res.redirect('/checkout/Concluido');
+    }
 }
--- routes/checkout.js
+++ routes/checkout.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const session = require('../middleware/session');
 
-
-const { Frete, Tipo_Pagamento, ListaProduto, Pedido } = require('../database/models');
-
 const checkoutController = require('../controllers/checkoutController')
 
 router.post('/carrinhoDeCompra', checkoutController.salvarProduto )
@@ -30,56 +27,12 @@ router.post('/frete', (req, res, next) => {
     res.redirect('/checkout/pagamento');
 })
 
-router.get('/pagamento', async function(req, res, next) {
-    const { produtos, frete } = req.session;
+router.get('/pagamento', checkoutController.somarTotalPedido);
 
-    const dadosFrete = await Frete.findOne({ 
-        where: { 
-            id_frete: frete
-        }
-    })
+router.post('/pagamento', checkoutController.pedido);
 
-    let totalProdutos = 0;
-
-    produtos.forEach(produto => {
-        totalProdutos += produto.valor * produto.quantidade;
-    })
-
-    const totalGeral = dadosFrete.valor + totalProdutos;
-    req.session.totalGeral = totalGeral;
-
-    res.render("pagamento", { title: "Forma de pagamento", totalProdutos, totalGeral, frete: dadosFrete.valor });
-});
-
-router.post('/pagamento', async (req, res, next) => {
-    req.session.pagamento = req.body.radio;
-
-        console.log(req.session)
-    const novoPedido = await Pedido.create({ 
-        id_cliente: req.session.usuario.id_cliente,
-        id_pagamento: req.session.pagamento,
-        id_endereco: req.session.endereco.id_endereco,
-        id_frete: req.session.frete,
-        valor: req.session.totalGeral
-    })
-
-    req.session.produtos.forEach(async produto => {
-        const total = produto.valor * produto.quantidade;
-
-        await ListaProduto.create({
-            id_pedido: novoPedido.id_pedido,
-            id_produto: produto.id_produto,
-            quantidade: produto.quantidade,
-            valor: total
-        }).catch(console.trace)
-    })
-
-    res.send('deu certo')
-    // res.redirect('/checkout/sucesso');
-})
-
-router.get('/sucesso', (req, res, next) => {
-    res.render('sucesso');
+router.get('/concluido', (req, res, next) => {
+    res.render('pedidoConcluido', { title: "pedidoConcluido"});
 })
 
 
