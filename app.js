179979f9781bd08efc1659531c54756dddd804ed
GabrielMiranda21 <gabrielmirand204@gmail.com>
2026-05-19 21:57:41 -0300

    ajuste para deploy

--- app.js
+++ app.js
@@ -30,4 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
 module.exports = app;
