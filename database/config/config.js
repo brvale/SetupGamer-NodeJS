b0f916963ace86c63d33bde6665b8adf6f0a4495
GabrielMiranda21 <gabrielmirand204@gmail.com>
2026-05-19 22:21:05 -0300

    fix: add variable db and deploy

--- database/config/config.js
+++ database/config/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-
 module.exports = {
   development: {
     username: process.env.DB_USER,
@@ -9,5 +8,19 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-}
+};
